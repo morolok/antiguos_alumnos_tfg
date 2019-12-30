574959671db249642197fcf237de38333db96532
Carlos <carlosmatauniversidad@gmail.com>
2019-12-30 19:46:40 +0100

    Cambio en el formato del fichero para que se vea bien

--- antiguos_alumnos_tfg/views.py
+++ antiguos_alumnos_tfg/views.py
@@ -5,40 +5,32 @@ from django.shortcuts import render
 
 
 def saludo(request):
-
     return HttpResponse("Pagina del TFG")
 
 
 def inicio(request):
-
 	return render(request, "index.html")
 
 
 def asociacion(request):
-
     return render(request, "asociacion.html")
 
 
 def actividades(request):
-
     return render(request, "actividades.html")
 
 
 def noticias(request):
-
     return render(request, "noticias.html")
 
 
 def empleo(request):
-
     return render(request, "empleo.html")
 
 
 def revistaIngenio(request):
-
     return render(request, "revistaIngenio.html")
 
 
 def multimedia(request):
-
     return render(request, "multimedia.html")
